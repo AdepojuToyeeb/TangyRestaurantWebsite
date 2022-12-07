c8454cb48a515fedb0fcb5e66f34c32251e1e121
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-07 07:52:23 +0100

    Created Coupons Controller plus index action method

---
+++ TangyRestaurantWebsite/Controllers/CouponsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TangyRestaurantWebsite.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TangyRestaurantWebsite.Controllers
+{
+    public class CouponsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupons.ToListAsync());
+        }
+    }
+}
+
