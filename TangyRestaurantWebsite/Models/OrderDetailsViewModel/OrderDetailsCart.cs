36e4da7995a424165339cbd98fc389c949dead44
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-11 15:01:07 +0100

    added Cart Details View Model

---
+++ TangyRestaurantWebsite/Models/OrderDetailsViewModel/OrderDetailsCart.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TangyRestaurantWebsite.Models.OrderDetailsViewModel
+{
+	public class OrderDetailsCart
+	{
+        public List<ShoppingCart>? listCart { get; set; }
+        public OrderHeader? OrderHeader { get; set; }
+    }
+}
+
