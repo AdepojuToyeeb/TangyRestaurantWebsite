4f92ce7ea5a62c9d79ef8af8c4a0273ccea90d0f
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-11 15:04:06 +0100

    added Shopping Cart Get Action Method

---
+++ TangyRestaurantWebsite/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TangyRestaurantWebsite.Data;
+using TangyRestaurantWebsite.Models;
+using TangyRestaurantWebsite.Models.OrderDetailsViewModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TangyRestaurantWebsite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderDetailsCart detailCart { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            detailCart = new OrderDetailsCart()
+            {
+                OrderHeader = new OrderHeader()
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+
+            if (cart != null)
+            {
+                detailCart.listCart = cart.ToList();
+            }
+
+            foreach (var list in detailCart.listCart)
+            {
+                list.MenuItem = _db.MenuItems.FirstOrDefault(m => m.Id == list.MenuItemId);
+                detailCart.OrderHeader.OrderTotal = detailCart.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
+
+                if (list.MenuItem.Description.Length > 100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            detailCart.OrderHeader.PickUpTime = DateTime.Now;
+
+            return View(detailCart);
+        }
+    
+}
+}
+
