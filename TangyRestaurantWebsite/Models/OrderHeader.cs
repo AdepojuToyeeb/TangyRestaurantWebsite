b15c53eda7ccd640ba114a87c359ff57d4f16d02
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-11 14:52:46 +0100

    Add Order Header Model

---
+++ TangyRestaurantWebsite/Models/OrderHeader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TangyRestaurantWebsite.Models
+{
+	public class OrderHeader
+	{
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser? ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        public DateTime PickUpTime { get; set; }
+
+        public string? CouponCode { get; set; }
+        public string? Status { get; set; }
+        public string? Comments { get; set; }
+    }
+}
+
