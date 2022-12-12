93bfab000e87c2d3be66ce83a048c701a89f7971
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-12 15:45:44 +0100

    Created the “download order details”

---
+++ TangyRestaurantWebsite/Models/OrderDetailsViewModel/OrderExportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TangyRestaurantWebsite.Models.OrderDetailsViewModel
+{
+	public class OrderExportViewModel
+	{
+        [Display(Name = "Start Date")]
+        public DateTime startDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime endDate { get; set; }
+    }
+}
+
