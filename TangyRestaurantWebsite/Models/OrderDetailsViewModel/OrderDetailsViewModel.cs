b4c78078cd5ea1ff4430ee68a9f4e04195197769
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-12 12:15:01 +0100

    order details view model

---
+++ TangyRestaurantWebsite/Models/OrderDetailsViewModel/OrderDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TangyRestaurantWebsite.Models.OrderDetailsViewModel
+{
+	public class OrderDetailsViewModel
+	{
+        public OrderHeader? OrderHeader { get; set; }
+        public List<OrderDetails>? OrderDetail { get; set; }
+    }
+}
+
