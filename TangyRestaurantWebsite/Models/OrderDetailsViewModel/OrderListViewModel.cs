c87a856570d8ff7a9c5e896acfa24aeec1662289
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-12 15:58:24 +0100

    Consuming Paging Tag Helper

--- TangyRestaurantWebsite/Models/OrderDetailsViewModel/OrderListViewModel.cs
+++ TangyRestaurantWebsite/Models/OrderDetailsViewModel/OrderListViewModel.cs
@@ -3,8 +3,8 @@ namespace TangyRestaurantWebsite.Models.OrderDetailsViewModel
 {
 	public class OrderListViewModel
 	{
-        public IList<OrderDetailsViewModel> Orders { get; set; }
-        public PagingInfo PagingInfo { get; set; }
+        public IList<OrderDetailsViewModel>? Orders { get; set; }
+        public PagingInfo? PagingInfo { get; set; }
     }
 }
 
