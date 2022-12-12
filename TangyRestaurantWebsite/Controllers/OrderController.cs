aa963f149839303e0aec88a34d5e22c3b614e8b5
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-12-12 12:26:19 +0100

    Order confirmation GET action

---
+++ TangyRestaurantWebsite/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TangyRestaurantWebsite.Data;
+using TangyRestaurantWebsite.Models.OrderDetailsViewModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TangyRestaurantWebsite.Controllers
+{
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        //CONFIRM GET
+        [Authorize]
+        public async Task<IActionResult> Confirm(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+
+            OrderDetailsViewModel OrderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = _db.OrderHeader.Where(o => o.Id == id && o.UserId == claim.Value).FirstOrDefault(),
+                OrderDetail = _db.OrderDetail.Where(o => o.OrderId == id).ToList()
+            };
+
+            return View(OrderDetailsViewModel);
+        }
+    }
+}
+
